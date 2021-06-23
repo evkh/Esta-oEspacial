f7658d568eaa3825101e42fbb064c433da64b462
evkh <gijow@live.com>
2021-06-23 17:59:52 -0300

    A

---
+++ EstacaoEspacial-main/NavMeshStationStarter/Assets/AIControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour
+{
+    //criando a var do NavMeshAgent
+    public NavMeshAgent agent;
+
+    private void Start()
+    {
+        //pegando o component NavMeshAgent
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+}
---
+++ EstacaoEspacial-main/NavMeshStationStarter/Assets/AgenteManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgenteManager : MonoBehaviour
+{
+    //Array para guardar os gameObjects agents
+    GameObject[] agents;
+
+    private void Start()
+    {
+        //setando a var agents com tudo que tiver a tag "ai"
+        agents = GameObject.FindGameObjectsWithTag("ai");
+    }
+
+    private void Update()
+    {   
+        //ao clicar com o mousebutton 0
+        if(Input.GetMouseButton(0))
+        {
+            //Instanciando o RaycastHit
+            RaycastHit hit;
+
+            //O raycast atinge algum objeto e seta o caminho do agente
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),out hit,100))
+            {
+                //para cada gameobject que estiver no array agents, caminha ate o destino
+                  foreach(GameObject a in agents)
+                {
+                    //seta o destino do agente
+                    a.GetComponent<AIControl>().agent.SetDestination(hit.point);
+                }
+
+            }
+        }
+    }
+}
